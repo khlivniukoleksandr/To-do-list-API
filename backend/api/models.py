8547ce1654f8a9f588f9345780b121638c5a85b5
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-10-14 19:07:53 +0300

    Initializing project

---
+++ backend/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ("pending", "Pending"),
+        ("in-progress", "In Progress"),
+        ("completed", "Completed"),
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="PENDING")
+
